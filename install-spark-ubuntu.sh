0b6f15ca893f2a595adb8cb2dba6c0f3c502bd5c
Karl Prior <karl.prior@groupimd.com>
2016-02-22 12:39:20 +0000

    updated spark install

--- install-spark-ubuntu.sh
+++ install-spark-ubuntu.sh
@@ -1,4 +1,5 @@
 sudo wget http://apache.mirror.anlx.net/spark/spark-1.6.0/spark-1.6.0.tgz -P /usr/local/src/
 sudo mkdir /usr/local/src/spark
 sudo tar xvf /usr/local/src/spark-1.6.0.tgz -C /usr/local/src/spark/
-sudo /usr/local/src/spark/spark-1.6.0/sbt/sbt assembly
+cd /usr/local/src/spark/spark-1.6.0/
+sudo build/sbt assembly
